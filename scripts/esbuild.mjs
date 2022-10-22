3af47b9a413ead98eb52cc67ae1e761ece1909dd
noriyotcp <noriyo.akita@gmail.com>
2022-10-22 12:37:11 +0900

    MOdify esbuild.mjs
    Fix entry point from js to ts

--- scripts/esbuild.mjs
+++ scripts/esbuild.mjs
@@ -1,13 +1,15 @@
 import { build } from 'esbuild';
-import { glob } from "glob";
+import pkg from "glob";
+const { glob } = pkg;
 
-const entryPoints = glob.sync('src/**/*.js');
+const entryPoints = glob.sync('src/**/*.ts');
 
 build({
   entryPoints,
   outbase: './src',
   outdir: './lib',
   platform: 'node',
+  sourcemap: true,
   bundle: true,
   watch: true,
 }).catch(() => process.exit(1))
