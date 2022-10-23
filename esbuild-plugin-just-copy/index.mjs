ae806c647d0e86cec033a6c6cf35a5ac009a73ed
noriyotcp <noriyo.akita@gmail.com>
2022-10-23 23:06:09 +0900

    Enable to copy directories recursively from glob path
    single file copying is broken

--- esbuild-plugin-just-copy/index.mjs
+++ esbuild-plugin-just-copy/index.mjs
@@ -1,4 +1,7 @@
 import fs from "node:fs";
+import glob from "glob";
+import { mkdir } from "node:fs/promises";
+import path from "node:path";
 
 export const justCopy = (options) => {
   const errors = [];
@@ -17,10 +20,49 @@ export const justCopy = (options) => {
     return stat.isFile();
   };
 
+  const composeToDirs = (rawFrom, rawTo) => {
+    const { dir } = path.parse(rawFrom);
+
+    if (!dir.endsWith === "/**") {
+      return; // TODO: throw error
+    }
+
+    const startFragment = dir.replace(`/**`, "");
+    console.log(startFragment);
+    // absolute paths include file name
+    // const fromPaths = glob.sync(from, { absolute: true });
+    const fromPaths = glob.sync(from);
+    console.log(fromPaths);
+    const replaced = fromPaths.map((fromPath) => {
+      return fromPath.replace(startFragment, rawTo);
+    });
+    // console.log(replaced);
+    // objects
+    const parsedReplacedPaths = replaced.map((_path) => path.parse(_path));
+    return parsedReplacedPaths.map((parsedPath) => {
+      if (parsedPath.ext === "") {
+        return path.join(parsedPath.dir, parsedPath.base);
+      }
+    }).filter((dir) => dir !== undefined);
+  };
+
+  const absoluteDirs = (absPaths) => {
+    return absPaths.map((p) => path.dirname(p));
+  }
+
   return {
     name: 'just-copy',
     setup(build) {
       build.onLoad({ filter: /.*/ }, async (args) => {
+        for (const path of composeToDirs(from, to)) {
+          try {
+            const createDir = await mkdir(path, { recursive: true });
+            console.log(`created ${createDir}`);
+          } catch (err) {
+            console.error(err.message);
+          }
+        }
+
         if (!isFile(from)) {
           errors.push({ text: `${from} is not a file` });
           return {
