7e861f0e8a3d89cd52c453729491b380122e3c43
noriyotcp <noriyo.akita@gmail.com>
2022-11-05 00:06:20 +0900

    Something's wrong, but error handling is ok

--- esbuild-plugin-just-copy/index.mjs
+++ esbuild-plugin-just-copy/index.mjs
@@ -1,7 +1,13 @@
-import fs from "node:fs";
+import fs, { mkdirSync } from "node:fs";
 import { mkdir } from "node:fs/promises";
 import { join, dirname } from "node:path";
-import { isGlob, sourceDirectories, pairsOfDirectories } from "./composers.mjs";
+import {
+  isGlob,
+  sourceDirectories,
+  pairsOfDirectories,
+  sourceFiles,
+  pairOfFiles,
+} from "./composers.mjs";
 
 export const justCopy = (options) => {
   const errors = [];
@@ -29,7 +35,7 @@ export const justCopy = (options) => {
           const sourceDirs = sourceDirectories(from);
           pairsOfDirectories({ sourceDirs, distDir: to }).forEach(
               ({ dist }) => {
-                mkdir(dist, { recursive: true }, (err) => {
+                mkdirSync(dist, { recursive: true }, (err) => {
                   if (err) {
                     errors.push(err.message);
                   }
@@ -37,6 +43,17 @@ export const justCopy = (options) => {
               }
             );
           // copy files
+          try {
+            sourceDirs.forEach((sourceDir) => {
+              const { dir, files } = sourceFiles(sourceDir);
+              pairOfFiles({ dir, files }, to).forEach(({ source, dist }) => {
+                fs.copyFileSync(source, dist);
+              });
+            });
+          } catch (err) {
+            errors.push({ text: err.message });
+          }
+          return { errors: errors };
         } else { // copy file
           if (!isFile(from)) {
             errors.push({ text: `${from} is not a file` });
